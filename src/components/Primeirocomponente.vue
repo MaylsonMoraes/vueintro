b7fe83e1fb24a4c5e475ce5217480c9c2c4a3aa7
Maylson <maylsonmoraesd@hotmail.com>
2022-01-04 09:08:51 -0300

    multiplos eventos

--- src/components/Primeirocomponente.vue
+++ src/components/Primeirocomponente.vue
@@ -1,12 +1,16 @@
 <template>
     <h1>Olá Vue!</h1>
     <p>Meu nome é {{nome}} e trabalho como {{profissao}}.</p>
+    <multiplosEventos />
 </template>
 
 <script>
-
+import multiplosEventos from './multiplosEventos.vue';
 export default {
     name: 'Primeirocomponente',
+    components: {
+        multiplosEventos
+    },
     data() {
         return{
             nome:"Maylson",
---
+++ src/components/multiplosEventos.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button @click="primeiro($event), segundo ($event), terceiro($event)">Ativar múltiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'multiplosEventos',
+    data() {
+        return {
+            multiplos_eventos:""
+        }
+    },
+    methods: {
+        primeiro(e) {
+            this.multiplos_eventos = "Primeiro evento ativado!"
+        },
+        segundo(e) {
+            setTimeout(() => this.multiplos_eventos ="Segundo evento ativado!", 500)
+        },
+        terceiro(e) {
+            setTimeout(() => this.multiplos_eventos ="", 1500)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        margin: 50px;
+    }
+</style>
