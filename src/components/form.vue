7c99f950da3cfa9579bf56acb1038b7310e30341
Maylson <maylsonmoraesd@hotmail.com>
2022-01-04 07:37:54 -0300

    eventos vue

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,16 +1,19 @@
 <template>
-    <form action="">
+    <form action="" @submit="enviarFormulario($event)">
         <h2>Meu formulário:</h2>
         <div>
-        <inputText />
+        <input type="text" v-model="name">
         </div>
         <div>
-        <inputText />
+        <input type="text" v-model="email">
         </div>
         <div>
         <Customsubmit /> 
         </div>
     </form>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>
 </template>
 <script>
 import inputText from './Form/inputText'
@@ -21,6 +24,33 @@ export default {
     components: {
         inputText,
         Customsubmit
+    },
+    data() {
+        return {
+            name:"",
+            email:""
+        }
+    },
+    methods: {
+        enviarFormulario(e) {
+            e.preventDefault();
+            const name = this.name;
+            const email = this.email;
+
+        console.log("formulario enviado!");
+        console.log("O nome é:"+ name);
+        console.log("O email é:"+ email);
+        
+        // ajax
+        // inserir no banco de dados
+
+        }
     }
 }
 </script>
+
+<style scoped>
+p {
+    margin: 150px;
+}
+</style>
