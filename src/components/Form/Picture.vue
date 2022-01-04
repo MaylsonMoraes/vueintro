88b6b5c98a5f7e068123d193b6255919e70a3bbb
Maylson <maylsonmoraesd@hotmail.com>
2022-01-04 09:31:04 -0300

    reutilização de componentes

--- src/components/Form/Picture.vue
+++ src/components/Form/Picture.vue
@@ -1,11 +1,18 @@
 <template>
     <img :src="avatar" :alt="descricao">
+    <reutilizacao />
 </template>
 <script>
+import reutilizacao from '../reutilizacao.vue'
+
 export default {
     name:'Picture',
+    components: {
+        reutilizacao
+    },
     data() {
-        return {
+  
+          return {
             avatar:"/img/download.jpeg",
             descricao: "Teste"
         }
--- src/components/Primeirocomponente.vue
+++ src/components/Primeirocomponente.vue
@@ -2,14 +2,18 @@
     <h1>Olá Vue!</h1>
     <p>Meu nome é {{nome}} e trabalho como {{profissao}}.</p>
     <multiplosEventos />
+    <reutilizacao />
 </template>
 
 <script>
 import multiplosEventos from './multiplosEventos.vue';
+import reutilizacao from './reutilizacao.vue';
 export default {
     name: 'Primeirocomponente',
     components: {
-        multiplosEventos
+        multiplosEventos,
+        reutilizacao,
+        
     },
     data() {
         return{
